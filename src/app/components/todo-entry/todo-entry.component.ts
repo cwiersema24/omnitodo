b2dda67ef64cc5147ac65d7cfe77ab80a23e7e26
Cora <coraw24@gmail.com>
2020-10-05 10:27:35 -0600

    Before Lunch Mon

---
+++ src/app/components/todo-entry/todo-entry.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+
+@Component({
+  selector: 'app-todo-entry',
+  templateUrl: './todo-entry.component.html',
+  styleUrls: ['./todo-entry.component.scss']
+})
+export class TodoEntryComponent implements OnInit {
+
+  form: FormGroup;
+  constructor(private bottomSheetRef: MatBottomSheetRef<TodoEntryComponent>,
+    private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(200)]],
+      project: [],
+      dueDate: []
+    });
+  }
+  cancel(): void {
+    this.bottomSheetRef.dismiss();
+  }
+  submit(): void {
+    this.bottomSheetRef.dismiss();
+  }
+
+}
